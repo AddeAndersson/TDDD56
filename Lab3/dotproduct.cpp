bc33550d6ee3bc33d294223be8960824078c9564
adran117 <adran117@olympen1-106.ad.liu.se>
2020-11-25 11:46:19 +0100

    Finished task 1

--- Lab3/dotproduct.cpp
+++ Lab3/dotproduct.cpp
@@ -10,16 +10,15 @@
 #include <skepu>
 
 /* SkePU user functions */
-
-/*
-float userfunction(...)
+float multiply(float a, float b)
 {
-	// your code here
+	return a * b;
 }
 
-// more user functions...
-
-*/
+float add(float a, float b)
+{
+	return a + b;
+}
 
 
 int main(int argc, const char* argv[])
@@ -29,41 +28,42 @@ int main(int argc, const char* argv[])
 		std::cout << "Usage: " << argv[0] << " <input size> <backend>\n";
 		exit(1);
 	}
-	
+
 	const size_t size = std::stoul(argv[1]);
 	auto spec = skepu::BackendSpec{argv[2]};
 //	spec.setCPUThreads(<integer value>);
 	skepu::setGlobalBackendSpec(spec);
-	
-	
+
+
 	/* Skeleton instances */
-//	auto instance = skepu::Map(userfunction);
-// ...
-	
+	auto CombMap = skepu::MapReduce<2>(multiply, add);
+	auto MultMap = skepu::Map<2>(multiply);
+	auto AddReduce = skepu::Reduce(add);
+
 	/* SkePU containers */
-	skepu::Vector<float> v1(size, 1.0f), v2(size, 2.0f);
-	
-	
+	skepu::Vector<float> v1(size, 1.0f), v2(size, 2.0f), v3(size);
+
+
 	/* Compute and measure time */
 	float resComb, resSep;
-	
+
 	auto timeComb = skepu::benchmark::measureExecTime([&]
 	{
-		// your code here
+		resComb = CombMap(v1, v2);
 	});
-	
+
 	auto timeSep = skepu::benchmark::measureExecTime([&]
 	{
-		// your code here
+		MultMap(v3, v1, v2);
+		resSep = AddReduce(v3);
 	});
-	
+
 	std::cout << "Time Combined: " << (timeComb.count() / 10E6) << " seconds.\n";
-	std::cout << "Time Separate: " << ( timeSep.count() / 10E6) << " seconds.\n";
-	
-	
+	std::cout << "Time Separate: " << (timeSep.count() / 10E6) << " seconds.\n";
+
+
 	std::cout << "Result Combined: " << resComb << "\n";
 	std::cout << "Result Separate: " << resSep  << "\n";
-	
+
 	return 0;
 }
-
